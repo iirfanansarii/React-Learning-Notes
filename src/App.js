3d6c945796ad3f5f03aa2a6eb1b2269991ad16db
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-13 00:05:26 +0530

    4.Card list component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import "./App.css";
 import { Component } from "react";
 
-
+import {CardList} from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
      monsters:[]
     };
   }
 
   /* component life cycle method */
-componentDidMount(){
+  componentDidMount(){
   fetch("https://jsonplaceholder.typicode.com/users")
   .then(response =>response.json())
   .then(users => this.setState({ monsters:users }));
   
 }
 
-
-
 /* map is a mehtod which iterate all those method inside an array eg:monsters */
 /* render is a method inside compoenent so when we extends react components method we get render method */
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => 
-          <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList name="bakait">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card-list.styles.css';
+
+/*props 
+*About:props is a boject of Cardlist and it would also have chiledren otherwise it would be null
+*Children: Anything or element we write in between Cardlist would be considered  the children of props
+*eg:   <CardList name="bakait"> hello  </CardList>  in app.js file here hello is children of props
+*/
+
+export const CardList = props =>{
+return  <div className="card-list">{props.children}</div>;
+}
